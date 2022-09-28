1be923311be0e4c5dfef537a15f3b9e92f66d976
Bharti Garde <bharti.garde@bluepineapple.io>
2022-09-28 15:15:30 +0530

    first part

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 function onSave() {
   const inputElement = document.getElementById("budget");
   const amount = inputElement.value;
+
+  // check if input is not empty
   if (!amount) {
     alert("Please enter your budget.");
     return;
   }
+
+  // check if the input is valid number
+  if (isNaN(amount)) {
+    alert("Enter a valid number.");
+    inputElement.value = "";
+    return;
+  }
+
+  // display amount on UI
   document.getElementById("amount").innerText = amount;
 
+  // disable the input field
   inputElement.setAttribute('disabled', '');
 }
